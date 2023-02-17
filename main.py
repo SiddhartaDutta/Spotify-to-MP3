5aee38cc2a707edbfbf6c85bdee9d25f62aa49e5
SiddhartaDutta <siddhartadutta99@gmail.com>
2023-02-16 18:50:14 -0800

    spotipy import working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import yt_dlp
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 import os
 import json
@@ -7,37 +8,14 @@ import webbrowser
 
 load_dotenv()
 
-username = os.environ["USERNAME"]
-clientID = os.environ["CLIENTID"]
-clientSecret = os.environ["CLIENTSECRET"]
-redirect_uri = 'http://google.com/callback/'
+auth_manager = SpotifyClientCredentials(client_id=os.environ.get("CLIENTID"), client_secret=os.environ.get("CLIENTSECRET"))
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
-oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirect_uri)
-token_dict = oauth_object.get_access_token()
-token = token_dict['access_token']
-spotifyObject = spotipy.Spotify(auth=token)
-user_name = spotifyObject.current_user()
-  
-# To print the JSON response from 
-# browser in a readable format.
-# optional can be removed
-print(json.dumps(user_name, sort_keys=True, indent=4))
-  
-while True:
-    print("Welcome to the project, " + user_name['display_name'])
-    print("0 - Exit the console")
-    print("1 - Search for a Song")
-    user_input = int(input("Enter Your Choice: "))
-    if user_input == 1:
-        search_song = input("Enter the song name: ")
-        results = spotifyObject.search(search_song, 1, 0, "track")
-        songs_dict = results['tracks']
-        song_items = songs_dict['items']
-        song = song_items[0]['external_urls']['spotify']
-        webbrowser.open(song)
-        print('Song has opened in your browser.')
-    elif user_input == 0:
-        print("Good Bye, Have a great day!")
-        break
+playlists = sp.user_playlists('spotify')
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlist['offset'], playlist['uri'], playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
     else:
-        print("Please enter valid user-input.")
+        playlists = None
