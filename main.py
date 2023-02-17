7e88eea803b05a0521b7680e7892ff6ae2bcda93
SiddhartaDutta <siddhartadutta99@gmail.com>
2023-02-16 19:02:36 -0800

    Working test script

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import yt_dlp
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+#from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy import SpotifyOAuth
 from dotenv import load_dotenv
 import os
 import json
@@ -8,14 +9,22 @@ import webbrowser
 
 load_dotenv()
 
-auth_manager = SpotifyClientCredentials(client_id=os.environ.get("CLIENTID"), client_secret=os.environ.get("CLIENTSECRET"))
-sp = spotipy.Spotify(auth_manager=auth_manager)
+# auth_manager = SpotifyClientCredentials(client_id=os.environ.get("CLIENTID"), client_secret=os.environ.get("CLIENTSECRET"))
+# sp = spotipy.Spotify(auth_manager=auth_manager, scope='playlist-read-private')
 
-playlists = sp.user_playlists('spotify')
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlist['offset'], playlist['uri'], playlist['name']))
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ.get("CLIENTID"),
+                                               client_secret=os.environ.get("CLIENTSECRET"),
+                                               redirect_uri="http://localhost:1234/",
+                                               scope="user-library-read"))
+
+taylor_uri = 'spotify:artist:06HL4z0CvFAxyc27GXpf02'
+
+results = sp.artist_albums(taylor_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = sp.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
