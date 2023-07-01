d18894d6ae30077fc9b62e52b6f5b859a117c99c
SiddhartaDutta <siddhartadutta99@gmail.com>
2023-06-30 23:05:51 -0700

    Added file deletion

--- setup.sh
+++ setup.sh
@@ -12,6 +12,23 @@ python3 ./setup.py
 # Build program image
 docker build -f Dockerfile -t spotify-to-mp3 .
 
+# Delete downloaded files
+rm main.py
+rm osScripts.py
+rm program.py
+rm setup.py
+rm setup.sh
+rm spotifyScripts
+
+rm LICENSE
+rm Dockerfile
+
+# Delete created files
+rm .env
+rm .cache
+
+rm -rf .__pycache__/
+
 # Run program image
 docker run --name spotify_to_mp3 -it --mount type=bind,src=$CURRENTPATH,target=/src spotify-to-mp3
 
