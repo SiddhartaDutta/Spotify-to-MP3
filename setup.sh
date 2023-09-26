4d3202f802d495ed05df02ce8471bc31ca765ace
SiddhartaDutta <siddhartadutta99@gmail.com>
2023-09-25 23:43:27 -0700

    Removed need for spotipy module prior to install

--- setup.sh
+++ setup.sh
@@ -7,9 +7,6 @@ CURRENTPATH=$(pwd)
 CURRENTUSER=${SUDO_USER}
 
 # Setup .env file
-echo "[UPDATE] Installing spotipy (installation will be removed in future update). This process may take several minutes..."
-yes "yes" | pip3 install spotipy
-echo "[UPDATE] spotipy installation complete."
 echo "[UPDATE] Initiating setup..."
 python3 ./setup.py
 
@@ -21,21 +18,21 @@ docker run --name spotify_to_mp3 -it --mount type=bind,src=$CURRENTPATH,target=/
 
 echo "[UPDATE] Deleting unnecessary files..."
 
-# # Delete downloaded files
-# rm main.py
-# rm osScripts.py
-# rm program.py
-# rm setup.py
-# rm setup.sh
-# rm spotifyScripts
+# Delete downloaded files
+rm main.py
+rm osScripts.py
+rm program.py
+rm setup.py
+rm setup.sh
+rm spotifyScripts
 
-# rm LICENSE
-# rm Dockerfile
+rm LICENSE
+rm Dockerfile
 
-# # Delete created files
-# rm .env
-# rm .cache*
+# Delete created files
+rm .env
+rm .cache*
 
-# rm -rf .__pycache__/
+rm -rf .__pycache__/
 
 echo "[UPDATE] Complete."
