03efe42f9b25d221f82745500fcc25f5f0a8b2f6
SiddhartaDutta <siddhartadutta99@gmail.com>
2024-01-03 04:01:11 -0800

    Changed from .env to json

--- setup.py
+++ setup.py
@@ -1,14 +1,40 @@
+import json
 from time import sleep
 
 def initialSetup():
 
     print("[INFO] The following will setup Spotify-to-MP3 for you.")
     sleep(0.5)
-    print("[INFO] Please enter the information as requested...")
+    print("[INFO] Please enter the information as requested. All mistakes can be fixed later in settings...")
     sleep(0.5)
 
-    clientID = str(input("[INPUT] Please input your generated Client ID: "))
-    clientSecret = str(input("[INPUT] Please input your generated Client Secret: "))
+    data = {"DEBUGMODE":"False",
+            "UPDATEIBROADCAST":'',
+            "SPOTIFYCLIENTID":'',
+            "SPOTIFYCLIENTSECRET":'',
+            "IBROADCASTUSER":'',
+            "IBROADCASTPSWD":'',
+            "NATIVEPLAYLISTS":{},
+            "IBROADCASTPLAYLISTS":{}
+            }
+
+    data["SPOTIFYCLIENTID"] = str(input("[INPUT] Please input your generated Spotify Client ID: "))
+    data["SPOTIFYCLIENTSECRET"] = str(input("[INPUT] Please input your generated Spotify Client Secret: "))
+
+    # Get iBroadcast information
+    run = True
+    while run:
+        promptAns = str(input('[INPUT] Would you like to connect to iBroadcast? Type \'Y\' for "Yes" or \'N\' for "No": '))
+        if(promptAns.lower() == 'n' or promptAns.lower() == 'no'):
+            data["UPDATEIBROADCAST"] = 'False'
+            run = False
+        elif(promptAns.lower() == 'y' or promptAns.lower() == 'yes'):
+            data["UPDATEIBROADCAST"] = 'True'
+            run = False
+        else:
+            print('[ERROR] Invalid Input.')
+
+    iBroadUpdate = data["UPDATEIBROADCAST"] == 'True'
 
     # Get number of playlists to be added
     numOfPlaylists = None
@@ -17,61 +43,41 @@ def initialSetup():
             numOfPlaylists = int(input("[INPUT] Please input the number of Spotify playlists you wish to update: "))
         except:
             print("[ERROR] Invalid input. Please input a number.")
+    
+    if iBroadUpdate:
+        data["IBROADCASTUSER"] = str(input("[INPUT] Please enter your iBroadcast username: "))
+        data["IBROADCASTPSWD"] = str(input("[INPUT] Please enter your iBroadcast password: "))
 
-    # Get playlist IDs
-    playlistIDs = []
-    while(len(playlistIDs)) < numOfPlaylists:
-        playlistIDs.append(str(input('[INPUT] Please input a playlist ID for playlist #' + str(len(playlistIDs)+1) + ': ')))
+    # Get playlist IDs and download counts
+    while(len(data["NATIVEPLAYLISTS"])) < numOfPlaylists:
 
-    # Get playlist downloaded
-    playlistLengths = []
-    while len(playlistLengths) < numOfPlaylists:
-        try:
-            playlistLengths.append(int(input('[INPUT] Please enter the current number of downloaded songs for playlist with ID ' + playlistIDs[len(playlistLengths)] + ': ')))
-        except:
-            print("[ERROR] Invalid input. Please input a number (input '0' for non-downloaded playlists).")
-
-    # Format playlist arrays for env file addition
-    playlistLengthStr = playlistIDStr = '['
-    for Length, ID in zip(playlistLengths, playlistIDs):
-
-        # Process length
-        tempStr = str(Length)
-        tempStr = '"' + tempStr + '",'
-        playlistLengthStr += tempStr
-
-        # Process ID
-        tempStr = str(ID)
-        tempStr = '"' + tempStr + '",'
-        playlistIDStr += tempStr
-
-    # Remove extra comma
-    if len(playlistIDs) == 0:
-        playlistLengthStr = playlistLengthStr + ']'
-        playlistIDStr = playlistIDStr + ']'
-    else:
-        playlistLengthStr = playlistLengthStr[:len(playlistLengthStr)-1] + ']'
-        playlistIDStr = playlistIDStr[:len(playlistIDStr)-1] + ']'
+        # Get Spotify playlist ID
+        spotifyID = str(input('[INPUT] Please input a playlist ID for playlist #' + str(len(data["NATIVEPLAYLISTS"]) + 1) + ': '))
         
-    # Set .env file
-    with open(".env", "w") as envFile:
+        # Get download count
+        run = True
+        while run:
+            try:
+                downloadCount = str(int(input('[INPUT] Please input the number of songs already downloaded for the above playlist: ')))
+                run = False
+            except:
+                print("[ERROR] Invalid input. Please input a number.")
 
-        # Debug Mode flag
-        envFile.write("DEBUGMODE=\'" + 'True' + '\'\n')
+        # Update native
+        data["NATIVEPLAYLISTS"].update({spotifyID : downloadCount})
 
-        # Update clientid
-        envFile.write("CLIENTID=\'" + clientID + '\'\n')
+        # Update iBroadcast
+        if iBroadUpdate:
+            data["IBROADCASTPLAYLISTS"].update({spotifyID : str(input('[INPUT] Please enter the corresponding iBroadcast playlist ID for the above playlist: '))})
+        else:
+            data["IBROADCASTPLAYLISTS"].update({spotifyID : ''})
 
-        # Update clientsecret
-        envFile.write("CLIENTSECRET=\'" + clientSecret + '\'\n')
-
-        # Update amplaylistlengths
-        envFile.write("AMPLAYLISTLENGTHS=" + playlistLengthStr + '\n')
-
-        # Update playlists
-        envFile.write("PLAYLISTS=" + playlistIDStr)
-
-    print('[Update] Finished setting up your environment file.')
+    # Set .data.json file
+    with open(".data.json", "w") as datafile:
+        obj = json.dumps(data, indent= 4)
+        datafile.write(obj)
+        
+    print('[Update] Finished setting up your data file.')
 
     print('[Update] Setup complete. Starting program...')  
 
